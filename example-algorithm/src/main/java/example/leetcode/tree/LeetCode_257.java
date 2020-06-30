47b30b5a2d42eb84214aae486135643823bdf87a
henryyu92 <1401802687@qq.com>
2020-06-30 22:32:56 +0800

    update

--- example-algorithm/src/main/java/example/leetcode/tree/LeetCode_257.java
+++ example-algorithm/src/main/java/example/leetcode/tree/LeetCode_257.java
@@ -1,4 +1,42 @@
 package example.leetcode.tree;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @see <a href=https://leetcode-cn.com/problems/binary-tree-paths/</a>
+ */
 public class LeetCode_257 {
+
+
+    public List<String> binaryTreePath(TreeNode root){
+        return null;
+    }
+
+    public List<String> recur(TreeNode root){
+        List<String> res = new ArrayList<>();
+
+        if (root == null){
+            return res;
+        }
+
+        recurProcess(root, "", res);
+
+        return res;
+    }
+
+    public void recurProcess(TreeNode node, String path, List<String> paths){
+        if (node == null){
+            return;
+        }
+        path += node.val;
+        if (node.left == null && node.right == null){
+            paths.add(path);
+            return;
+        }
+        path += "->";
+        recurProcess(node.left, path, paths);
+        recurProcess(node.right, path, paths);
+    }
+
 }
--- example-algorithm/src/main/java/example/leetcode/tree/TreeNode.java
+++ example-algorithm/src/main/java/example/leetcode/tree/TreeNode.java
@@ -1,4 +1,7 @@
 package example.leetcode.tree;
 
 public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
 }
