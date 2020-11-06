d487839f18581040fac7ed9f347a61a3b2055272
henryyu92 <1401802687@qq.com>
2020-11-06 22:25:56 +0800

    update

--- basic-net/basic-grpc/src/main/java/example/errorhanding/ErrorHandingClient.java
+++ basic-net/basic-grpc/src/main/java/example/errorhanding/ErrorHandingClient.java
@@ -1,16 +1,23 @@
 package example.errorhanding;
 
 import com.google.common.base.Verify;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.Uninterruptibles;
 import example.helloworld.GreeterGrpc;
-import example.helloworld.GreeterImpl;
 import example.helloworld.HelloReply;
 import example.helloworld.HelloRequest;
 import io.grpc.*;
 import io.grpc.stub.StreamObserver;
 
+import javax.annotation.Nullable;
 import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+import static com.google.common.util.concurrent.MoreExecutors.directExecutor;
 
 public class ErrorHandingClient {
 
@@ -41,7 +48,7 @@ public class ErrorHandingClient {
     private void blockingCall(){
         GreeterGrpc.GreeterBlockingStub stub = GreeterGrpc.newBlockingStub(channel);
         try{
-            stub.sayHello(HelloRequest.newBuilder().setName("Bart").build())
+            stub.sayHello(HelloRequest.newBuilder().setName("Bart").build());
         }catch (Exception e){
             Status status = Status.fromThrowable(e);
             Verify.verify(status.getCode() == Status.Code.INTERNAL);
@@ -65,6 +72,90 @@ public class ErrorHandingClient {
             Verify.verify(status.getCode() == Status.Code.INTERNAL);
             Verify.verify(status.getDescription().contains("Xerxes"));
             // Cause is not transmitted over the wire.
+        }
+    }
+
+    void futureCallback(){
+        GreeterGrpc.GreeterFutureStub stub = GreeterGrpc.newFutureStub(channel);
+        ListenableFuture<HelloReply> response = stub.sayHello(HelloRequest.newBuilder().setName("Maggie").build());
+
+        final CountDownLatch latch = new CountDownLatch(1);
+        Futures.addCallback(response, new FutureCallback<HelloReply>() {
+            @Override
+            public void onSuccess(@Nullable HelloReply result) {
+
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                Status status = Status.fromThrowable(t);
+                Verify.verify(status.getCode() == Status.Code.INTERNAL);
+                Verify.verify(status.getDescription().contains("Crybaby"));
+                // Cause is not transmitted over the wire..
+                latch.countDown();
+            }
+        }, directExecutor());
+
+        if (!Uninterruptibles.awaitUninterruptibly(latch, 1, TimeUnit.SECONDS)) {
+            throw new RuntimeException("timeout!");
+        }
+    }
+
+    void asyncCall(){
+        GreeterGrpc.GreeterStub stub = GreeterGrpc.newStub(channel);
+        HelloRequest request = HelloRequest.newBuilder().setName("Homer").build();
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        StreamObserver<HelloReply> responseObserver = new StreamObserver<HelloReply>(){
+
+            @Override
+            public void onNext(HelloReply helloReply) {
+
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                Status status = Status.fromThrowable(throwable);
+                Verify.verify(status.getCode() == Status.Code.INTERNAL);
+                Verify.verify(status.getDescription().contains("Overbite"));
+                // Cause is not transmitted over the wire..
+                latch.countDown();
+            }
+
+            @Override
+            public void onCompleted() {
+
+            }
+        };
+        stub.sayHello(request, responseObserver);
+
+        if (!Uninterruptibles.awaitUninterruptibly(latch, 1, TimeUnit.SECONDS)) {
+            throw new RuntimeException("timeout!");
+        }
+    }
+
+    void advancedAsyncCall() {
+        ClientCall<HelloRequest, HelloReply> call =
+                channel.newCall(GreeterGrpc.getSayHelloMethod(), CallOptions.DEFAULT);
+
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        call.start(new ClientCall.Listener<HelloReply>() {
+
+            @Override
+            public void onClose(Status status, Metadata trailers) {
+                Verify.verify(status.getCode() == Status.Code.INTERNAL);
+                Verify.verify(status.getDescription().contains("Narwhal"));
+                // Cause is not transmitted over the wire.
+                latch.countDown();
+            }
+        }, new Metadata());
+
+        call.sendMessage(HelloRequest.newBuilder().setName("Marge").build());
+        call.halfClose();
+
+        if (!Uninterruptibles.awaitUninterruptibly(latch, 1, TimeUnit.SECONDS)) {
+            throw new RuntimeException("timeout!");
         }
     }
 }
